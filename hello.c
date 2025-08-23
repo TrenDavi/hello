5e43198d2d406c6af6bb5fe21de088da257a742b
Trent Davidson <tdavidson16@huskers.unl.edu>
2025-08-23 11:44:10 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Trent Davidson
+ *         tdavidson16@huskers.unl.edu
+ * Date: 2025/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Trent Davidson\n");
+
+  return 0;
+}
